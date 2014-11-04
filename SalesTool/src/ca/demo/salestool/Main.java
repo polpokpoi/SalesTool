99130e487fb75043ac6074150b1b37f520a011f0
hyeongcheol <polpokpoi@naver.com>
2014-11-05 01:47:47 +0900

    Initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,22 @@
+package ca.demo.salestool;
+
+public class Main {
+   public static void main(String arg[]){
+	   
+	SalesData data = new SalesData();
+	displayGreeting();
+	data.display();
+	
+}
+	
+ private static void displayGreeting(){
+	 
+ 	 System.out.println("HELLO HAPPY SALES PEOPLE!");
+	 System.out.println("THIS APP SHOWS SALES DATA");
+	 
+	 
+	 
+ }
+	
+	
+}
